b778d73c059880eb80bbf2997564d1f2c81d2856
yeongcheolkim <39392544+yeongcheolkim@users.noreply.github.com>
2021-04-19 19:27:54 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -7,9 +7,9 @@ module.exports = {
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Dev.Yeongcheol',
+  domain: 'dev.yckim.io',
+  author: 'Yeong Cheol Kim',
 
   // open graph metadata (optional)
   description: 'Example site description',
