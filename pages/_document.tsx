09a811482f3d96ce0f9c0a34002a634d08dd42ad
KimYeongCheol <asa03003@gmail.com>
2021-04-21 15:38:12 +0900

    #6 : 노션 페이지 관련 수정
    * 페이지 노출 부분 수정
    - 트위터관련 제거
    - 옵션 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,51 +1,48 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { IconContext } from 'react-icons'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
-        <Html lang='en'>
-          <Head>
-            <link rel='shortcut icon' href='/favicon.png' />
+      <Html>
+        <Head>
+          <meta
+            name='google-site-verification'
+            content='efjMNdIz7NsOOkbzUAEMvhjwXi-K7u7dLCpyY4zE49M'
+          />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `
+            }}
+          />
+          <link
+            rel='icon'
+            href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✏️</text></svg>'
+          />
+          <link rel='manifest' href='/manifest.json' />
+        </Head>
 
-            <link
-              rel='apple-touch-icon'
-              sizes='180x180'
-              href='/apple-touch-icon.png'
-            />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='96x96'
-              href='/favicon-96x96.png'
-            />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='32x32'
-              href='/favicon-32x32.png'
-            />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='16x16'
-              href='/favicon-16x16.png'
-            />
+        <body>
+          <script src='noflash.js' />
 
-            <link rel='manifest' href='/manifest.json' />
-          </Head>
+          <Main />
 
-          <body>
-            <script src='noflash.js' />
-
-            <Main />
-
-            <NextScript />
-          </body>
-        </Html>
-      </IconContext.Provider>
+          <NextScript />
+        </body>
+      </Html>
     )
   }
 }
